0a8896955cbe9a17b045f5f2646d4078926e3a83
azimuthal-pro <emmanuelquarshie171@gmail.com>
2026-04-28 13:20:54 +0000

    update to inventory model

--- Inventory/low_stock_report_export.php
+++ Inventory/low_stock_report_export.php
@@ -8,16 +8,13 @@ header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="low_stock_report.csv"');
 
 $output = fopen('php://output', 'w');
-fputcsv($output, ['Item Name', 'Item Code', 'Unit Price (GHS)', 'Quantity in Stock', 'Minimum Stock Level', 'Date Added']);
+fputcsv($output, ['Item Name', 'Quantity in Stock', 'Minimum Stock Level']);
 
 foreach ($lowStockItems as $item) {
     fputcsv($output, [
         $item['item'],
-        $item['item_code'],
-        number_format($item['unit_price'], 2),
         $item['quantity_in_stock'],
         $item['min_stock_level'],
-        date('Y-m-d', strtotime($item['created_at']))
     ]);
 }
 
